38449ce934d6fc363fb8c525835807321c3f3cc9
Joseg <l20920129@voaxaca.tecnm.mx>
2024-03-07 20:05:53 -0600

    ..

--- js/scritp.js
+++ js/scritp.js
@@ -1,20 +1,18 @@
 const inputPassword = document.getElementById('input-password')
+const spanValidacion = document.getElementById("validacion")
 
 document.getElementById('iniciar').addEventListener('click', (e)=>{
     e.preventDefault()
     const password= inputPassword.value
     var regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,}$/;
 
+    if(regex.test(password) ){
     window.location.href = 'view/menu.html';
-
-    // if(regex.test(password) ){
-    // window.location.href = 'view/menu.html';
-    // console.log("iniciando boton")
-
-    // }else{
-    //     alert("el dato es menor de 8 dijitos")
-    // }
+    console.log("iniciando boton")
+    }else{
+        spanValidacion.textContent = "El dato es incorrecto"
+        
+    }
     
     
 })
-

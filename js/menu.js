aa2da9927bc96e775b4d633157f1828dbd754997
Joseg <l20920129@voaxaca.tecnm.mx>
2024-03-08 22:47:11 -0600

    ...

--- js/menu.js
+++ js/menu.js
@@ -1,23 +1,23 @@
 const divContenido = document.getElementById("contenido-menu");
 
 const cambiarMenu = (opcion) => {
-    divContenido.innerHTML = "";
-  
-    if (opcion === "caja") {
-        agregarVideo("4CMOII_oiU0");
-    } else if (opcion === "responsivo") {
-        agregarVideo("yneuaVjotO8");
-    } else if (opcion === "framework") {
-        agregarVideo("TmltkTIAX0c");
-    }
+  divContenido.innerHTML = "";
+
+  if (opcion === "caja") {
+    agregarVideo("4CMOII_oiU0");
+  } else if (opcion === "responsivo") {
+    agregarVideo("yneuaVjotO8");
+  } else if (opcion === "framework") {
+    agregarVideo("TmltkTIAX0c");
+  }
 };
 
 const agregarVideo = (videoId) => {
-    const iframeElement = document.createElement("iframe");
-    iframeElement.width = 640;
-    iframeElement.height = 360;
-    iframeElement.src = `https://www.youtube.com/embed/${videoId}`;
-    iframeElement.allowfullscreen = true;
+  const iframeElement = document.createElement("iframe");
+  iframeElement.width = 640;
+  iframeElement.height = 360;
+  iframeElement.src = `https://www.youtube.com/embed/${videoId}`;
+  iframeElement.allowfullscreen = true;
 
-    divContenido.appendChild(iframeElement);
+  divContenido.appendChild(iframeElement);
 };
